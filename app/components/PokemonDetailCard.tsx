b7d6ccd923c1c169cc4829d3ea8b481ec02e099a
Bonham <gswjppj@gmail.com>
2023-11-15 14:07:05 +0900

    remove unused files

--- app/components/PokemonDetailCard.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react";
-
-export default function PokemonDetailCard() {
-  return <div>detail</div>;
-}
--- app/lib/getPokemonDetail.tsx
+++
@@ -1,3 +0,0 @@
-export default function getPokemonDetail(name: string) {
-  return <div></div>;
-}
